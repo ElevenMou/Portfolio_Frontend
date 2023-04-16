4c002ef305cc6bc5eaa6099b6f2509ae12cead4a
moussa saidi <moussa.saidi.01@gmail.com>
2023-04-16 18:32:53 +0000

    build i18n

--- src/i18n.js
+++ src/i18n.js
@@ -4,7 +4,7 @@ import { initReactI18next } from 'react-i18next';
 i18n
     .use(initReactI18next)
     .init({
-        debug: true,
+        debug: false,
         fallbackLng: 'en',
         interpolation: {
             escapeValue: false,
